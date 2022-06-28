bce3424a559c8f67356fb1bcc74e880d7d3a87fc
ramisha-anwer <ramishaaa27@gmail.com>
2022-06-28 21:27:06 +0500

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}
+    style= {{backgroundColor:props.color}} className='btn'>{props.text}</button>
+  )
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+function Header({title}) {
+    const onClick= () => {
+        console.log("clicked")
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color="steelblue" text= "Add Task" onClick={onClick}/>
+        </header>
+    )
+}
+
+Header.defaultProps={
+    title: "Task Tracker"
+}
+Header.propTypes = {
+    title:PropTypes.string
+}
+
+export default Header
